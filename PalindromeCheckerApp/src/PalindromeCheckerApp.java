1b1c89cd89ee0053cf7db46c509465c34933f3d9
dhananjaykumar077 <dhananjaykumar89059@gmail.com>
2026-03-12 14:51:33 +0530

    Implemented Use Case 6 - Queue Stack Palindrome Check

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -4,20 +4,22 @@ import java.util.LinkedList;
 import java.util.Stack;
 
 /**
- * MAIN CLASS UseCase6 Palindrome Checker App
+ * MAIN CLASS UseCase6PalindromeCheckerApp
+ *
  * Use Case 6: Queue Stack Fairness Check
  *
  * Description:
  * This class demonstrates palindrome validation using
  * two different data structures:
- * Queue (FIFO – First In First Out)
- * Stack (LIFO – Last In First Out)
+ * Queue (FIFO - First In First Out)
+ * Stack (LIFO - Last In First Out)
  *
  * Characters are inserted into both structures and then
  * compared by removing from the front of the queue and
  * the top of the stack.
  *
- * If all characters match, the string is a palindrome.
+ * If all characters match, the input string is confirmed
+ * as a palindrome.
  *
  * @author Developer
  * @version 6.0
@@ -26,9 +28,10 @@ import java.util.Stack;
 public class PalindromeCheckerApp {
 
     /**
-     * Application entry point for UC6
-     * @param args Command-Line arguments
+     * Application entry point for UC6.
+     * @param args Command-line arguments
      */
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -40,17 +43,20 @@ public class PalindromeCheckerApp {
         Stack<Character> stack = new Stack<>();
 
         // Insert characters into queue and stack
-        for (char c : input.toCharArray()) {
-            queue.add(c);
-            stack.push(c);
+        for (char ch : input.toCharArray()) {
+            queue.add(ch);
+            stack.push(ch);
         }
 
         boolean isPalindrome = true;
 
-        // Compare characters
+        // Compare queue and stack values
         while (!queue.isEmpty()) {
 
-            if (!queue.remove().equals(stack.pop())) {
+            char qChar = queue.remove();
+            char sChar = stack.pop();
+
+            if (qChar != sChar) {
                 isPalindrome = false;
                 break;
             }
